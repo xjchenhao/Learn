c30c7be39e680aacd743601a6f7556455a775710
xjchenhao <wy.chenhao@qq.com>
2015-06-27 21:34:11 +0800

    express+mongodb

--- nodejs/站点建设/imooc/app.js
+++ nodejs/站点建设/imooc/app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var _ = require('underscore');
+
+var Movie = require('./models/movie');
 
 var port = process.env.PORT || 3000;
 var app = express();
+
+//连接本地数据库
+mongoose.connect('mongodb://localhost/imooc');
 
 //视图文件的路径
 app.set('views', './views/pages');
@@ -10,13 +18,9 @@ app.set('views', './views/pages');
 //使用的模板引擎
 app.set('view engine', 'jade');
 
-/*
- * 格式化表单
- * 新版express已不支持app.use(express.bodyParser());
- * 需要安装body-parser模块，npm install body-parser
- * 然后使用代码为：app.use(require('body-parser').urlencoded({extended: true}))
- * */
-//app.use(express.bodyParser());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //静态资源的路径
 app.use(express.static(path.join(__dirname, 'bower_components')));
@@ -29,87 +33,122 @@ console.log('imooc started on port ' + port);
 
 //页面路由
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: 'imoooc 首页',
-        movies: [{
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg'
-        }, {
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg'
-        }, {
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg'
-        }, {
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg'
-        }, {
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg'
-        }]
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+
+        res.render('index', {
+            title: 'imooc 首页',
+            movies: movies
+        });
     });
 });
 
 app.get('/list', function (req, res) {
     res.render('list', {
         title: 'imoooc 列表页',
-        movies:[{
-            _id:1,
+        movies: [{
+            _id: 1,
             title: '机械战警',
-            doctor:'何塞.帕迪利亚',
-            year:2014,
-            country:'美国',
-            language:'英语',
+            doctor: '何塞.帕迪利亚',
+            year: 2014,
+            country: '美国',
+            language: '英语',
             poster: 'http://tu1.xiamp4.com/20140210202546153.jpg',
             flash: 'http://player.youku.com/player.php/sid/XNJA1Njc0NTUy/v.swf',
-            summary:'我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述'
-        },{
-            _id:1,
+            summary: '我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述'
+        }, {
+            _id: 1,
             title: '机械战警',
-            doctor:'何塞.帕迪利亚',
-            year:2014,
-            country:'美国',
-            language:'英语',
+            doctor: '何塞.帕迪利亚',
+            year: 2014,
+            country: '美国',
+            language: '英语',
             poster: 'http://tu1.xiamp4.com/20140210202546153.jpg',
             flash: 'http://player.youku.com/player.php/sid/XNJA1Njc0NTUy/v.swf',
-            summary:'我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述'
+            summary: '我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述'
         }]
     });
 });
 
 app.get('/movie/:id', function (req, res) {
-    res.render('detail', {
-        title: 'imoooc 详情页',
-        movie: {
-            title: '机械战警',
-            doctor:'何塞.帕迪利亚',
-            year:2014,
-            country:'美国',
-            language:'英语',
-            poster: 'http://tu1.xiamp4.com/20140210202546153.jpg',
-            flash: 'http://player.youku.com/player.php/sid/XNJA1Njc0NTUy/v.swf',
-            summary:'我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述我是描述'
-        }
+    var id = req.params.id;
+
+    Movie.findById(id, function (err, movie) {
+        res.render('detail', {
+            title: 'imooc ' + movie.title,
+            movie: movie
+        });
     });
 });
 
+//admin update movie
+app.get('/admin/update/:id', function (req, res) {
+    var id = req.params.id;
+
+    if (id) {
+        Movie.findById(id, function (err, movie) {
+            res.render('admin', {
+                title: 'imooc 后台更新',
+                movie: movie
+            });
+        });
+    }
+});
+
+//admin post movie
+app.post('/admin/movie/new', function (res, req) {
+    var id = res.body.movie._id;
+    var movieObj = req.body.movie;
+    var _movie;
+    if (id !== 'undefined') {
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+
+            _movie = _.extend(movie, movieObj);
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err);
+                }
+
+                res.redirect('/movie/' + movie._id);
+            });
+        });
+    } else {
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        });
+
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+
+            res.redirect('/movie/' + movie._id);
+        });
+    }
+});
+
+//list page
 app.get('/admin/list', function (req, res) {
-    res.render('admin', {
-        title: 'imoooc 后台录入页',
-        movie:{
-            title: '',
-            doctor:'',
-            year:'',
-            country:'',
-            language:'',
-            poster: '',
-            flash: '',
-            summary:''
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
         }
+
+        res.render('index', {
+            title: 'imooc 列表页',
+            movies: movies
+        });
     });
 });
---
+++ nodejs/站点建设/imooc/schemas/movie.js
@@ -0,0 +1,49 @@
+var mongoose = require('mongoose');
+var MovieSchema = new mongoose.Schema({
+    doctor: String,
+    title: String,
+    language: String,
+    country: String,
+    summary: String,
+    flash: String,
+    poster: String,
+    year: Number,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+
+//每次在存储数据之前都会来调用一下这个方法
+MovieSchema.pre('save', function () {
+    //数据是否是新添加的
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now()
+    } else {
+        this.meta.updateAt = Date.now()
+    }
+
+    next();
+});
+
+MovieSchema.statice = {
+    fetch: function (cd) { // 取出数据库里所有数据
+        return this
+            .find({})
+            .sort('meta.updateAt')
+            .exec(cd)
+    },
+    findById: function (id, cd) { // 取出数据库里所有数据
+        return this
+            .findOne({_id: id})
+            .exec(cd)
+    }
+};
+
+module.exports = MovieSchema;
