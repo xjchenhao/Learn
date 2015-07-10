8052b7524b69201472412dd06d619a40320cc3d9
JerryGong <jerryygon@gmail.com>
2015-07-10 08:00:19 +0800

    comment and modification from Jerry

--- nodejs/站点建设/imooc/app.js
+++ nodejs/站点建设/imooc/app.js
@@ -18,8 +18,10 @@ app.set('views', './views/pages');
 //使用的模板引擎
 app.set('view engine', 'jade');
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+// extended A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
+// 设置为false时只能解析 value 为 string 和 数组，为true的时候可以解析value 是任意的。
+// 我测试了下，用true 才能得到结果^^ 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 //静态资源的路径
@@ -102,6 +104,9 @@ app.post('/admin/movie/new', function (res, req) {
     var id = res.body.movie._id;
     var movieObj = req.body.movie;
     var _movie;
+    // 我卡在这好久，原来视频写的也是 !=='underfined'但是这表明id被建立但是没有赋值的情况。
+    // 最后改成 id!=="" 才顺利post成功。
+    // 其实if(id)就可以了，只要值为trusy就好，""本身就是falsy所以会建立 Movie
     if (id !== 'undefined') {
         Movie.findById(id, function (err, movie) {
             if (err) {
