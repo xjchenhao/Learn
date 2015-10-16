865e6857235fe33039234c8425c6d62d6f4f0e5f
xjchenhao <wy.chenhao@qq.com>
2015-10-16 16:40:49 +0800

    react demo

---
+++ react/demo12/browser.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var React = require('react');
+var App = require('./app');
+
+React.render(React.createElement(App, { items: window.APP_PROPS.items }), document.getElementById('content'));
