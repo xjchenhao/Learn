0fd92dbdb67046120d96f2e6b47c28ea21b9f17f
xjchenhao <wy.chenhao@qq.com>
2017-03-28 16:39:08 +0800

    egg 学习

---
+++ egg-example/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1490689099167_1208';
+
+  return config;
+};
---
+++ egg-example/egg-example/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1490689171498_6092';
+
+  return config;
+};
