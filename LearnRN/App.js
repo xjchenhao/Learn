40052718159f6c4f8bd3d1663d2d0b0a51469dd7
xjchenhao <wy.chenhao@qq.com>
2017-12-10 14:49:03 +0800

    《React Native跨平台移动应用开发》第二章demo

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -6,64 +6,83 @@
 
 import React, {Component} from 'react';
 import {
-  Platform,
+  AppRegistry,
   StyleSheet,
   Text,
   View,
   Dimensions,
-  PixelRatio
+  TextInput
 } from 'react-native';
-const {height, width} = Dimensions.get('window');
-const pixelRatio = PixelRatio.get();
 
+let widthOfMargin = Dimensions.get('window').width * 0.05;
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-  'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-  'Shake or press menu button for dev menu',
-});
+export default class LearnRN extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputedNum: '',
+      inputedPW: ''
+    };
+    this.updatePW = this.updatePW.bind(this);
+  }
 
-export default class App extends Component<{}> {
+  updateNum(newText) {
+    this.setState((state) => {
+      return {
+        inputedNum: newText
+      }
+    });
+  }
+
+  updatePW(newText) {
+    this.setState(() => {
+      return {
+        inputedPW: newText
+      }
+    });
+  }
+
   render() {
-    let aValue;
-    console.log('Render has been executed.');
-    console.log('Screen height is:' + height);
-    console.log('Screen width is:' + width);
-    console.log('aValue is:' + aValue);
-    console.log('The type of aValue is:' + typeof(aValue));
-
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          逻辑像素等于{pixelRatio}实际像素单位
+        <TextInput style={styles.textInputStyle} placeholder={'请输入手机号'}
+                   onChangeText={(newText)=>this.updateNum(newText)}
+        />
+        <Text style={styles.textPromptStyle}>
+          您输入的手机号：{this.state.inputedNum}
         </Text>
-        <Text style={styles.instructions}>
-          手机屏幕高度为{height}逻辑像素
-        </Text>
-        <Text style={styles.instructions}>
-          手机屏幕宽度为{width}逻辑像素
+        <TextInput style={styles.textInputStyle} placeholder={'请输入密码'} secureTextEntry={true}
+                   onChangeText={this.updatePW}
+        />
+        <Text style={styles.bigTextPrompt}>
+          确定
         </Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white'
   },
-  welcome: {
+  textInputStyle: {
+    margin: widthOfMargin,
+    backgroundColor: 'gray',
+    fontSize: 20
+  },
+  textPromptStyle: {
+    margin: widthOfMargin,
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  bigTextPrompt: {
+    margin: widthOfMargin,
+    backgroundColor: 'gray',
+    color: 'white',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    fontSize: 30,
+  }
 });
+
+// AppRegistry.registerComponent('LearnRN', () => LearnRN);
