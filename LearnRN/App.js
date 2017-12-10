cd7a10418ecaaeb8c20c512e23b29d9a0b24d7b0
xjchenhao <wy.chenhao@qq.com>
2017-12-10 14:08:09 +0800

    demo1

--- LearnRN/App.js
+++ LearnRN/App.js
@@ -4,33 +4,45 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  PixelRatio
 } from 'react-native';
+const {height, width} = Dimensions.get('window');
+const pixelRatio = PixelRatio.get();
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
   render() {
+    let aValue;
+    console.log('Render has been executed.');
+    console.log('Screen height is:' + height);
+    console.log('Screen width is:' + width);
+    console.log('aValue is:' + aValue);
+    console.log('The type of aValue is:' + typeof(aValue));
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          逻辑像素等于{pixelRatio}实际像素单位
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          手机屏幕高度为{height}逻辑像素
         </Text>
         <Text style={styles.instructions}>
-          {instructions}
+          手机屏幕宽度为{width}逻辑像素
         </Text>
       </View>
     );
